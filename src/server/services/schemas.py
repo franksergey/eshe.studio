c076aeb8d49efdb8d4b97031587b7961122345a1
Soucelover <mr.ktotov@gmail.com>
2026-09-12 18:44:50 +0300

    Make database and validation constraints more complex

--- src/server/services/schemas.py
+++ src/server/services/schemas.py
@@ -11,14 +11,18 @@ class CurrencyCode(StrEnum):
     EUR = "EUR"
 
 
+class MoneyType(BaseModel):
+    amount: Annotated[Decimal | None, Field(ge=Decimal(0))]
+    currency: CurrencyCode | None
+
+
 class SpecificationItem(BaseModel):
-    name: str
-    count: int
+    name: Annotated[str, Field(min_length=1, max_length=1024)]
+    count: Annotated[int, Field(ge=1, le=256)]
 
     link: HttpUrl | None = None
-    tags: list[str]
+    tags: Annotated[list[str], Field(max_length=16)]
 
-    price: Annotated[Decimal, Field(ge=Decimal(0))]
-    price_currency: CurrencyCode
+    price: MoneyType | None
 
     model_config = ConfigDict(use_enum_values=True)
--- src/server/sql/models.py
+++ src/server/sql/models.py
@@ -1,7 +1,14 @@
 from decimal import Decimal
 from typing import ClassVar
 
-from sqlalchemy import ForeignKey, MetaData, Numeric, SmallInteger, String
+from sqlalchemy import (
+    CheckConstraint,
+    ForeignKey,
+    MetaData,
+    Numeric,
+    SmallInteger,
+    String,
+)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from server.services.schemas import CurrencyCode
@@ -28,7 +35,11 @@ class SpecificationTagDB(Base):
         ForeignKey("specification_items.id"), primary_key=True
     )
     ordinal_no: Mapped[int] = mapped_column(primary_key=True)
+
     tag: Mapped[str] = mapped_column(String(255))
+
+
+PYDANTIC_MAX_URL_LENGTH = 2083
 
 
 class SpecificationItemDB(Base):
@@ -36,10 +47,20 @@ class SpecificationItemDB(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(String(1024))
     count: Mapped[int] = mapped_column(SmallInteger)
 
-    link: Mapped[str]
+    link: Mapped[str] = mapped_column(String(PYDANTIC_MAX_URL_LENGTH))
 
-    price: Mapped[Decimal] = mapped_column(Numeric(precision=12, scale=2))
-    price_currency: Mapped[CurrencyCode]
+    price: Mapped[Decimal | None] = mapped_column(
+        Numeric(precision=12, scale=2)
+    )
+    price_currency: Mapped[CurrencyCode | None]
+
+    __table_args__ = (
+        CheckConstraint(
+            "(price IS NULL AND price_currency IS NULL) "
+            "OR NOT(price IS NULL OR price_currency IS NULL)",
+            name="ux_c_email_or_phone_required",
+        ),
+    )
