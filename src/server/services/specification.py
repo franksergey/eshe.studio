263f18ad1eb8f0668ccf37542ba8d883f543de6e
Soucelover <mr.ktotov@gmail.com>
2026-09-23 11:07:46 +0300

    Rewrite port and service methods as asynchronous

--- src/server/services/specification.py
+++ src/server/services/specification.py
@@ -12,16 +12,16 @@ if TYPE_CHECKING:
 class SpecificationService:
     repo: AbstractSpecificationRepo
 
-    def get(self, id: int) -> Specification:
-        obj = self.repo.get(id)
+    async def get(self, id: int) -> Specification:
+        obj = await self.repo.get(id)
 
         if obj is None:
             raise SpecificationNotFoundError
 
         return obj
 
-    def get_all(self) -> list[Specification]:
-        return self.repo.get_all()
+    async def get_all(self) -> list[Specification]:
+        return await self.repo.get_all()
 
-    def get_all_pure(self) -> list[SpecificationPure]:
-        return self.repo.get_all_pure()
+    async def get_all_pure(self) -> list[SpecificationPure]:
+        return await self.repo.get_all_pure()
